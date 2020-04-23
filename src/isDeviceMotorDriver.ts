fd0dd92e63408546028691acba45e4f438d6965c
Cameron Tacklind <cameron@tacklind.com>
2020-04-23 03:01:37 -0700

    Check version number

--- src/isDeviceMotorDriver.ts
+++ src/isDeviceMotorDriver.ts
@@ -2,10 +2,22 @@ import { Device } from 'usb';
 
 const deviceVid = 0xdead;
 const devicePid = 0xbeef;
+const expectedVersion = 10;
 
 export function isDeviceMotorDriver(device: Device) {
   const dec = device.deviceDescriptor;
   const ven = dec.idVendor;
   const prod = dec.idProduct;
-  return ven == deviceVid && prod == devicePid;
+
+  if (ven != deviceVid) return false;
+  if (prod != devicePid) return false;
+
+  if (dec.bcdUSB != expectedVersion) {
+    console.log(
+      `Found ${dec.iSerialNumber} but has incorrect version: '${dec.bcdUSB}'. Expected: '${expectedVersion}'.`
+    );
+    return false;
+  }
+
+  return true;
 }
