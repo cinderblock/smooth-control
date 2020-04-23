c44d6914f0205adaee5b5ed93ae6f18cee708c2e
Cameron Tacklind <cameron@tacklind.com>
2020-04-23 03:55:24 -0700

    Fix version number check

--- src/isDeviceMotorDriver.ts
+++ src/isDeviceMotorDriver.ts
@@ -1,8 +1,18 @@
 import { Device } from 'usb';
+import { validateNumber } from './utils/validateNumber';
 
 const deviceVid = 0xdead;
 const devicePid = 0xbeef;
-const expectedVersion = 10;
+
+function versionBCD(major: number, minor: number, patch: number) {
+  validateNumber('major', major, 1 << 8);
+  validateNumber('minor', minor, 1 << 4);
+  validateNumber('patch', patch, 1 << 4);
+
+  return (major << 8) | (minor << 4) | patch;
+}
+
+const expectedVersion = versionBCD(0, 1, 0);
 
 export function isDeviceMotorDriver(device: Device) {
   const dec = device.deviceDescriptor;
@@ -12,9 +22,9 @@ export function isDeviceMotorDriver(device: Device) {
   if (ven != deviceVid) return false;
   if (prod != devicePid) return false;
 
-  if (dec.bcdUSB != expectedVersion) {
+  if (dec.bcdDevice != expectedVersion) {
     console.log(
-      `Found ${dec.iSerialNumber} but has incorrect version: '${dec.bcdUSB}'. Expected: '${expectedVersion}'.`
+      `Found ${dec.iSerialNumber} but has incorrect version: '${dec.bcdDevice}'. Expected: '${expectedVersion}'.`
     );
     return false;
   }
